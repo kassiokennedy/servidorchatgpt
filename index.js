d46f862ab68aa8e6a3ebd37f81e55710be162bc4
kassio kennedy <kassio1206@hotmail.com>
2023-05-15 18:42:38 -0300

    update

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const { Configuration, OpenAIApi } = require("openai");
 require('dotenv').config();
 
-
 const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
 });
@@ -38,8 +37,6 @@ const textGeneration = async (prompt) => {
 
 const webApp = express();
 
-const PORT = process.env.PORT;
-
 webApp.use(express.urlencoded({ extended: true }));
 webApp.use(express.json());
 webApp.use((req, res, next) => {
@@ -57,9 +54,15 @@ webApp.post('/dialogflow', async (req, res) => {
     
     let action = req.body.queryResult.action;
     let queryText = req.body.queryResult.queryText;
+    // let parameters = req.body.queryResult.parameters;
+    let comida1 = req.body.queryResult.parameters.comida1;
+    let comida2 = req.body.queryResult.parameters.comida2;
+    let comida3 = req.body.queryResult.parameters.comida3;
+    let alimento = req.body.queryResult.parameters.alimento;
 
     if (action === 'input.unknown') {
         let result = await textGeneration(queryText);
+
         if (result.status == 1) {
             res.send(
                 {
@@ -73,16 +76,177 @@ webApp.post('/dialogflow', async (req, res) => {
                 }
             );
         }
+    // ---------- SUGESTÔES DE RECEITA ----------------------------------------------
+    } else if (action == 'sugerirreceita') {
+        let result = await textGeneration('dê uma dica de prato para fazer com esses tres ingredientes' + comida1 + ' ' + comida2 + ' ' + comida3 );
+        console.log(result.response)
+
+        if (result.status == 1) {
+            res.send(
+                {
+                    fulfillmentText: result.response
+                }
+            );
+        } else {
+            res.send(
+                {
+                    fulfillmentText: `Sorry, I'm not able to help with that.`
+                }
+            );
+        }
+
+    }  else if (action == 'sugerirreceita.almoco') {
+        let result = await textGeneration('dê uma dica de almoço para fazer com esses tres ingredientes' + comida1 + ' ' + comida2 + ' ' + comida3 );
+        console.log("dica de almoço:")
+        console.log(result.response)
+
+        if (result.status == 1) {
+            res.send(
+                {
+                    fulfillmentText: result.response
+                }
+            );
+        } else {
+            res.send(
+                {
+                    fulfillmentText: `Sorry, I'm not able to help with that.`
+                }
+            );
+        }
+
+    } else if (action == 'sugerirreceita.lanche') {
+        let result = await textGeneration('dê uma dica de lanche para fazer com esses tres ingredientes' + comida1 + ' ' + comida2 + ' ' + comida3 );
+        console.log("dica de lanche:")
+        console.log(result.response)
+        if (result.status == 1) {
+            res.send(
+                {
+                    fulfillmentText: result.response
+                }
+            );
+        } else {
+            res.send(
+                {
+                    fulfillmentText: `Sorry, I'm not able to help with that.`
+                }
+            );
+        }
+
+    }  else if (action == 'sugerirreceita.sobremesa') {
+        let result = await textGeneration('dê uma dica de sobremesa para fazer com esses tres ingredientes' + comida1 + ' ' + comida2 + ' ' + comida3 );
+        console.log("Dica de sobremesa:")
+        console.log(result.response)
+        if (result.status == 1) {
+            res.send(
+                {
+                    fulfillmentText: result.response
+                }
+            );
+        } else {
+            res.send(
+                {
+                    fulfillmentText: `Sorry, I'm not able to help with that.`
+                }
+            );
+        }
+
+    } else if (action == 'sugerirreceita.qualquer') {
+        let result = await textGeneration('dê uma dica de comida pouco conhecida para que a eu possa experimentar');
+        console.log("\nDica de qualquer tipo de comida:")
+        console.log(result.response)
+        if (result.status == 1) {
+            res.send(
+                {
+                    fulfillmentText: result.response
+                }
+            );
+        } else {
+            res.send(
+                {
+                    fulfillmentText: `Sorry, I'm not able to help with that.`
+                }
+            );
+        }
+    // ---------- INDICAR A QUALIDADE DO ALIMENTO ----------------------------------------------
+    } else if (action == 'saber_se_alimento_ok') {
+        let result = await textGeneration('me dê dicas de de como o alimento' + alimento + ' se encontra quando esta em bom estado de consumo');
+        console.log("\nDica de qualidade:")
+        console.log(result.response)
+        if (result.status == 1) {
+            res.send(
+                {
+                    fulfillmentText: result.response
+                }
+            );
+        } else {
+            res.send(
+                {
+                    fulfillmentText: `Sorry, I'm not able to help with that.`
+                }
+            );
+        }
+    // ---------- SUGESTÔES DE RECEITA ----------------------------------------------
+    }  else if (action == 'saber_se_alimento_ok_naosei') {
+        let result = await textGeneration('me dê dicas de de como verificar a qualidade do alimento' + alimento + ' spara ajudar a indentificar quando esta em bom estado de consumo ou se nao presta mais');
+        console.log("\nDica de qualidade:")
+        console.log(result.response)
+        if (result.status == 1) {
+            res.send(
+                {
+                    fulfillmentText: result.response
+                }
+            );
+        } else {
+            res.send(
+                {
+                    fulfillmentText: `Sorry, I'm not able to help with that.`
+                }
+            );
+        }
+    // ---------- SUGESTÔES DE RECEITA ----------------------------------------------
     } else {
         res.send(
             {
                 fulfillmentText: `No handler for the action ${action}.`
             }
-        );
+        );    
     }
+
+    /**
+     *  else if (action === 'input.ajuda') {
+        valor = req.body.queryResult.parameters.ajuda
+        res.send(
+            {
+                fulfillmentText: `Muito obrigado! Para duvidas, sugestoes ou reclamações por favor mande um email para ajuda@email.com.`
+            }
+        );
+        // console.log('ajuda: '+ valor)
+        /*
+        if (valor == 'sim'){
+            res.send(
+                {
+                    fulfillmentText: `você disse sim!`
+                }
+            );
+        } else if (valor =='nao'){
+            res.send(
+                {
+                    fulfillmentText: `Muito obrigado! Para duvidas, sugestoes ou reclamações por favor mande um email para ajuda@email.com.`
+                }
+            );
+        } else {
+            res.send(
+                {
+                    fulfillmentText: `nao entendi sua resposta`
+                }
+            );
+        }
+        
+        // res.status(200)
+        // console.log("input.ajuda")
+    } 
+     */
 });
-
-
 
 /**
  * Rodar na porta 5000
